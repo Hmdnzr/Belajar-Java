244d4709c4b336a3d3d123a572c3b783dd355f46
Hamdan <100034314+Hmdnzr@users.noreply.github.com>
2022-06-11 19:32:54 +0700

    add file jdbc test

---
+++ jdbc_test/src/App.java
@@ -0,0 +1,8 @@
+import com.mysql.cj.protocol.x.SyncFlushDeflaterOutputStream;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        try
+    }
+    Class.forname("com.mysql.cj.jdbc.Driver");
+}
